15b2924d3013e2e839e000a1a38967f08febd102
Nick Hand <nhand@berkeley.edu>
2014-05-06 23:29:47 -0700

    updated quadrupole with new kcut extrapolation method which seems to be working

--- pyRSD/rsd/correlation.pyx
+++ pyRSD/rsd/correlation.pyx
@@ -74,13 +74,12 @@ class Correlation(object):
             logspline = InterpolatedUnivariateSpline(k, np.log(Pspec))
             slope = derivative(logspline, kcut, dx=1e-3)*kcut
             
-            print "power law slope = ", slope
-            
+            # now compute the combined model
             lowk_spline = InterpolatedUnivariateSpline(k, Pspec)
             k_total = np.logspace(np.log10(k.min()), np.log10(KMAX), 1000)
-            
             Pspec_total = (k_total <= kcut)*lowk_spline(k_total) + \
                           (k_total > kcut)*lowk_spline(kcut)*(k_total/kcut)**slope
+                          
         return k_total, Pspec_total
     #end _extrapolate_power
     
@@ -98,14 +97,14 @@ class Correlation(object):
         
         # first compute the linear correlation values
         linear_contrib = np.zeros(len(s))
-        if kmin < kmin_model:
+        if kmin < kmin_model or linear:
             
             if linear:
                 klin = self.power.integrals.klin
                 kmax = None
             else:
                 klin = np.logspace(kmin, kmin_model, 500)
-                kmax = self.power.k.min()
+                kmax = kmin_model
             integrals = _fourier_integrals.Fourier1D(0, kmin, smoothing_radius, 
                                                      klin, self._kaiser_monopole(klin),
                                                      kmax=kmax)
@@ -127,31 +126,46 @@ class Correlation(object):
     #end monopole
     
     #---------------------------------------------------------------------------
-    # def quadrupole(self, s, quad_func, smoothing_radius=0., kcut=0.2, linear=False):
-    #     """
-    #     Compute the monopole moment of the configuration space correlation 
-    #     function.
-    #     """
-    #     # compute the minimum wavenumber we need
-    #     kmin = 0.01 / np.amax(s)
-    #     
-    #     # do the extrapolation
-    #     if linear:
-    #         beta = self.power.f/self.power.b1
-    #         quad_linear = (4./3*beta + 4./7*beta**2) * self.power.b1**2 * self.power.integrals.Plin
-    #         
-    #         self.k_extrap = self.power.integrals.klin
-    #         self.P_extrap = quad_linear
-    #     else:
-    #         self.k_extrap, self.P_extrap = self._extrapolate_power(quad_func(self.power), 
-    #                                                                kmin, kcut, 2)
-    # 
-    #     # initialize the fourier integrals class
-    #     integrals = _fourier_integrals.Fourier1D(2, kmin, smoothing_radius, 
-    #                                              self.k_extrap, self.P_extrap)
-    # 
-    #     return -1.*integrals.evaluate(s) 
-    # #end quadrupole
+    def quadrupole(self, s, quad_func, smoothing_radius=0., kcut=0.2, linear=False):
+        """
+        Compute the monopole moment of the configuration space correlation 
+        function.
+        """
+        # compute the minimum wavenumber we need
+        kmin = 0.01 / np.amax(s) # integral converges for ks < 0.1
+        kmin_model = self.power.k.min() 
+        if kmin_model > 0.05: 
+            raise ValueError("Power spectrum must be computed down to at least k = 0.05 h/Mpc.")
+        
+        # first compute the linear correlation values
+        linear_contrib = np.zeros(len(s))
+        if kmin < kmin_model or linear:
+            
+            if linear:
+                klin = self.power.integrals.klin
+                kmax = None
+            else:
+                klin = np.logspace(kmin, kmin_model, 500)
+                kmax = kmin_model
+            integrals = _fourier_integrals.Fourier1D(2, kmin, smoothing_radius, 
+                                                     klin, self._kaiser_quadrupole(klin),
+                                                     kmax=kmax)
+            linear_contrib[:] = integrals.evaluate(s)[:]
+            
+            if linear:
+                return linear_contrib
+    
+        # now do the contribution from the full model
+        model_contrib = np.zeros(len(s))
+        
+        # do the high k power extrapolation
+        self.k_extrap, self.P_extrap = self._extrapolate_power(quad_func(self.power), kcut)
+        integrals = _fourier_integrals.Fourier1D(2, kmin_model, smoothing_radius, 
+                                                 self.k_extrap, self.P_extrap)
+        model_contrib[:] = integrals.evaluate(s)[:]
+                                                 
+        return linear_contrib + model_contrib 
+    #end quadrupole
     
     #---------------------------------------------------------------------------
     

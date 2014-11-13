19253d031fc2dc2f901f0fe136541ddbc3dafb41
Nick Hand <nhand@berkeley.edu>
2014-11-13 16:15:30 -0500

    stochasticity parameters determined from gaussian process for bias interpolation

--- pyRSD/rsd/tools.py
+++ pyRSD/rsd/tools.py
@@ -2,6 +2,8 @@ import numpy as np
 import bisect
 from scipy.interpolate import interp1d
 from scipy.integrate import simps
+import pandas as pd
+from sklearn.gaussian_process import GaussianProcess
 
 def extrap1d(interpolator):
     """
@@ -52,92 +54,90 @@ lambda_z1 = {'4.82': (-35181.3, -5473.79),
 lambda_z2 = {'4.64': (-16915.5, -3215.98), 
              '3.17': (-2661.11, -229.627), 
              '2.32': (-427.779, -41.3676)}              
-lambdas = {'0.000': lambda_z0, '0.509': lambda_z1, '0.989': lambda_z2}
+lambdas = [(0., lambda_z0), (0.509, lambda_z1), (0.989, lambda_z2)]
 
 def stochasticity(bias, z, return_nan=False):
     """
     Given a linear bias and redshift, return the stochasticity based on 
     the mean of simulation results.
     """
+    assert np.isscalar(z), 'Redshift input must be scalar'
+    
+    # first construct the pandas DataFrame
+    # make a data frame holding the lambda data from sims
+    keys = []
+    data = []
+    for red, params in lambdas:
+        for b in sorted(params.keys()):
+            keys.append((red, float(b)))
+            data.append(params[b])
+            
+    index = pd.MultiIndex.from_tuples(keys, names=['z', 'b1'])
+    data = pd.DataFrame(data, index=index, columns=['constant', 'slope'])
+    redshifts = np.array(data.index.levels[0])
+    
     # check redshift value
-    z_keys_str = sorted(lambdas.keys())
-    z_keys = np.array(z_keys_str, dtype=float)
-    if z > np.amax(z_keys): 
+    if z > np.amax(redshifts): 
         if return_nan: 
             return (np.nan, np.nan)
         else:
-            raise ValueError("Cannot determine stochasticity for z > %s" %np.amax(z_keys))
+            raise ValueError("Cannot determine stochasticity for z > %s" %np.amax(redshifts))
         
-    if z < np.amin(z_keys): 
+    if z < np.amin(redshifts): 
         if return_nan: 
             return (np.nan, np.nan)
         else:
-            raise ValueError("Cannot determine stochasticity for z < %s" %np.amin(z_keys))
+            raise ValueError("Cannot determine stochasticity for z < %s" %np.amin(redshifts))
 
     # determine the z indices
     redshift_lambdas = {}
-    if z in z_keys:
-        inds = np.where(z_keys == z)[0][0]
-        redshift_lambdas[str(z)] = lambdas[z_keys_str[inds]]
+    if z in redshifts:
+        redshift_lambdas[z] = data.xs(z, level=0)
     else:
         
-        index_zhi = bisect.bisect(z_keys, z)
+        index_zhi = bisect.bisect(redshifts, z)
         index_zlo = index_zhi - 1
-        zhi = z_keys_str[index_zhi]
-        zlo = z_keys_str[index_zlo]
+        zhi = redshifts[index_zhi]
+        zlo = redshifts[index_zlo]
         
-        redshift_lambdas[zlo] = lambdas[zlo]
-        redshift_lambdas[zhi] = lambdas[zhi]
+        redshift_lambdas[zlo] = data.xs(zlo, level=0)
+        redshift_lambdas[zhi] = data.xs(zhi, level=0)
 
     # now get the mean values for this bias, at this redshift
-    z_keys_str = sorted(redshift_lambdas.keys())
-    z_keys = np.array(z_keys_str, dtype=float)
     params = []
-    for z_key in z_keys_str:
-    
-        z_lambda = redshift_lambdas[z_key]
-        
-        bias_keys_str = sorted(z_lambda.keys())
-        bias_keys = np.array(bias_keys_str, dtype=float)
-        if bias > np.amax(bias_keys): 
-            if return_nan: 
-                return (np.nan, np.nan)
-            else:
-                raise ValueError("Cannot determine stochasticity for b > %s" %np.amax(bias_keys))
-        if bias < np.amin(bias_keys): 
-            if return_nan: 
-                return (np.nan, np.nan)
-            else:
-                raise ValueError("Cannot determine stochasticity for b < %s" %np.amin(bias_keys))
-        
-        
-        if bias in bias_keys:
-            inds = np.where(bias_keys == bias)[0][0]
-            lam  = z_lambda[bias_keys_str[inds]][0]
-            A    = z_lambda[bias_keys_str[inds]][1]
-        else:
+    for k, v in redshift_lambdas.iteritems():
+                  
+        # biases at this redshift
+        biases = np.array(v.index)
+            
+        # initialize the Gaussian Process
+        gp = GaussianProcess(corr='cubic', theta0=1e-2, thetaL=1e-4, thetaU=1e-1, random_start=100)
 
-            index_bhi = bisect.bisect(bias_keys, bias)
-            index_blo = index_bhi - 1
-            bhi = bias_keys_str[index_bhi]
-            blo = bias_keys_str[index_blo]
-    
-            # get the mean values
-            w = (bias - float(blo)) / (float(bhi) - float(blo))
-            lam = (1 - w)*z_lambda[blo][0] + w*z_lambda[bhi][0]
-            A = (1 - w)*z_lambda[blo][1] + w*z_lambda[bhi][1]
-    
-        params.append((lam, A))
+        # setup the fitting        
+        x = np.atleast_2d(biases).T
+        y_const = np.atleast_2d(v.constant).T
+        y_slope = np.atleast_2d(v.slope).T
+        
+        # do the fitting and prediction at this bias value
+        gp.fit(x, y_const)
+        constant = gp.predict(np.atleast_2d(bias).T)
+
+        gp.fit(x, y_slope)
+        slope = gp.predict(np.atleast_2d(bias).T)
+            
+        params.append((constant, slope))
         
     if len(params) == 1:
         return  params[0][0], params[0][1]
     else:
         
-        w = (z - z_keys[0]) / (z_keys[1] - z_keys[0]) 
-        lam = (1 - w)*params[0][0] + w*params[1][0]
-        A = (1 - w)*params[0][1] + w*params[1][1]
+        z_sorted = sorted(redshift_lambdas.keys())
+        zlo, zhi = z_sorted[0], z_sorted[1]
+        w = (z - zlo) / (zhi - zlo) 
+        constant = (1 - w)*params[0][0] + w*params[1][0]
+        slope = (1 - w)*params[0][1] + w*params[1][1]
     
-        return lam, A
+        return constant, slope
 #end stochasticity
 
 #-------------------------------------------------------------------------------

b1f5c619a326110ddfb3dda70f5edeb40cade0e2
Nick Hand <nhand@berkeley.edu>
2014-12-10 14:46:20 -0800

    old version of mu vectorize actually is faster...whoops

--- pyRSD/rsd/tools.py
+++ pyRSD/rsd/tools.py
@@ -414,10 +414,16 @@ def mu_vectorize(f):
     """ 
     def wrapper(self, *args):
         mu = args[0]
-        if np.isscalar(mu): 
-            return f(self, mu)
+        scalar = np.isscalar(mu)
+        if scalar: 
+            mu = [mu]
         else:
-            return np.vstack([f(self, imu) for imu in mu]).T
+            mu = np.asarray(mu)
+            if mu.ndim > 1: return f(self, mu)
+        mu = np.array([mu,]*len(self.k)).transpose()
+        
+        P_out = f(self, mu)
+        return P_out[0,:] if scalar else P_out.transpose()
         
     return wrapper
 #-------------------------------------------------------------------------------

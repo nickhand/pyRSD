b217a16fbc5d4a39a0870fb8980078b2fa5976d8
Nick Hand <nhand@berkeley.edu>
2015-02-13 13:38:47 -0800

    now using setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,23 +1,35 @@
 #! /usr/bin/env python
-from distutils.core import setup, Command
-from distutils.command.install import install as DistutilsInstall
-from setuptools import find_packages
+from setuptools import setup, Command, find_packages
+from setuptools.command.install import install as Install
 import os
 
+#-------------------------------------------------------------------------------
 # my own install class to make pygcl before installing
-class MyInstall(DistutilsInstall):
+class MyInstall(Install):
     
     def run(self):
-        ans = os.system("cd pyRSD/gcl; make gcl;")
-        if (ans > 0): raise ValueError("Failed to make `pygcl` module; installation cannot continue")
-        DistutilsInstall.run(self)
+        # check that setup.config exists
+        if not os.path.isfile('setup.config'):
+            msg = "Installation requires 'setup.config' file in base directory."
+            if os.path.isfile('setup.config.example'):
+                msg += " Try copying over 'setup.config.example' with the appropriate edits."
+            raise OSError(msg)
         
+        # make pygcl
+        ans = os.system("cd pyRSD/gcl; make gcl;")
+        if (ans > 0): raise ValueError("Failed to make `pygcl` module; installation cannot continue.")
+        
+        # run the python setup
+        Install.run(self)
+        
+        # copy over the shared object library to the installation directory
         print "copying gcl shared object library to install dir"
         args = self.install_libbase, self.config_vars['dist_name'] 
         install_path = "{}/{}/gcl/python".format(*args)
         ans = os.system("cp pyRSD/gcl/python/_gcl.so {}".format(install_path))
         if (ans > 0): raise ValueError("Error copying shared object libraries")
-        
+
+#-------------------------------------------------------------------------------        
 # my own command to do a clean of all necessary files      
 class MyClean(Command):
     description = "custom clean command that removes build directories and runs make clean on pygcl"
@@ -31,7 +43,7 @@ class MyClean(Command):
         os.system('rm -rf ./build')
         os.system('cd pyRSD/gcl; make clean;')
 
-
+#-------------------------------------------------------------------------------
 descr = """pyRSD
 
 Algorithms to compute the redshift space matter power spectra using 
@@ -46,7 +58,8 @@ MAINTAINER          = 'Nick Hand'
 MAINTAINER_EMAIL    = 'nicholas.adam.hand@gmail.com'
 VERSION             = '1.0'
 
-    
+#-------------------------------------------------------------------------------   
+scripts_dir = 'pyRSD/rsdfit/scripts'
 setup(  cmdclass={'install': MyInstall, 'clean' : MyClean},
         name=DISTNAME,
         description=DESCRIPTION,
@@ -55,5 +68,11 @@ setup(  cmdclass={'install': MyInstall, 'clean' : MyClean},
         maintainer_email=MAINTAINER_EMAIL,
         version=VERSION,
         packages=find_packages(),
-        scripts=['pyRSD/rsdfit/scripts/' + script for script in os.listdir('pyRSD/rsdfit/scripts')]
+        scripts=[os.path.join(scripts_dir, script) for script in os.listdir(scripts_dir)],
+        install_requires=[
+            'pandas', 
+            'scikit-learn',
+            'numpy',
+            'scipy'
+        ]
     )

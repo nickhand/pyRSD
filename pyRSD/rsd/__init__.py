f34561b05b733393d609c8e43e1ae083282dab3d
Nick Hand <nhand@berkeley.edu>
2015-08-11 14:17:49 -0700

    importing major power spectrum classes in __init__ for convenience

--- pyRSD/rsd/__init__.py
+++ pyRSD/rsd/__init__.py
@@ -1,10 +1,11 @@
 INTERP_KMIN = 1e-3
 INTERP_KMAX = 1.0
 
-import power_dm
-import power_biased
+from .power_dm import DarkMatterSpectrum
+from .power_biased import BiasedSpectrum
+from .power_halo import HaloSpectrum
+from .power_gal import GalaxySpectrum
 import correlation
-import power_halo
-import power_gal
+
 
 __all__ = ['power_dm', 'power_biased', 'power_halo', 'power_gal', 'correlation']

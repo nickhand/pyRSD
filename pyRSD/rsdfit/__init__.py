23b3dbc365db110bbcac937da2282b14a5a382bf
Nick Hand <nhand@berkeley.edu>
2015-11-08 21:26:27 -0800

    better imports of analysis and bestfit module

--- pyRSD/rsdfit/__init__.py
+++ pyRSD/rsdfit/__init__.py
@@ -14,6 +14,7 @@ import parameters
 import results
 import theory
 import util
+import analysis
 
 
 __all__ = []
--- pyRSD/rsdfit/analysis/__init__.py
+++ pyRSD/rsdfit/analysis/__init__.py
@@ -1,1 +1,2 @@
 from .driver import AnalysisDriver
+from .bestfit import BestfitParameterSet, to_comparison_table

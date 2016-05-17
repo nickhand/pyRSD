cf94b2802ffdccb3a8d1ed838eb63c8f68248b01
Nick Hand <nhand@berkeley.edu>
2016-05-17 15:07:22 -0700

    store the default tex names

--- pyRSD/rsdfit/analysis/__init__.py
+++ pyRSD/rsdfit/analysis/__init__.py
@@ -1,2 +1,33 @@
 from .driver import AnalysisDriver
 from .bestfit import BestfitParameterSet, to_comparison_table
+
+tex_names               = {}
+tex_names['Nsat_mult']  = r'$N_{s,\mathrm{mult}}$'
+tex_names['fsB']        = r'$f_{s_B}$'
+tex_names['sigma8_z']   = r'$\sigma_8$'
+tex_names['sigma_s']    = r'$\sigma_s$'
+tex_names['f1h_sBsB']   = r'$f_{1h, s_B s_B}$'
+tex_names['f1h_cBs']    = r'$f_{1h, c_B s}$'
+tex_names['fs']         = r'$f_s$'
+tex_names['f']          = r'$f$'
+tex_names['gamma_b1sB'] = r'$\gamma_{b_{1,s_B}}$'
+tex_names['gamma_b1sA'] = r'$\gamma_{b_{1,s_A}}$'
+tex_names['sigma_c']    = r'$\sigma_c$'
+tex_names['alpha_par']  = r'$\alpha_\parallel$'
+tex_names['alpha_perp'] = r'$\alpha_\perp$'
+tex_names['b1_cA']      = r'$b_{1,c_A}$'
+tex_names['sigma_sA']   = r'$\sigma_{s_A}$'
+tex_names['sigma_sB']   = r'$\sigma_{s_B}$'
+tex_names['b1_cB']      = r'$b_{1,c_B}$'
+tex_names['fcB']        = r'$f_{c_B}$'
+tex_names['b1_sB']      = r'$b_{1,s_B}$'
+tex_names['b1_sA']      = r'$b_{1,s_A}$'
+tex_names['NcBs']       = r'$N_{c_B s}$'
+tex_names['NsBsB']      = r'$N_{s_B s_B}$'
+tex_names['fsigma8']    = r'$f \sigma_8$'
+tex_names['b1_c']       = r'$b_{1, c}$'
+tex_names['b1']         = r'$b_1$'
+tex_names['b1_s']       = r'$b_{1,s}$',
+tex_names['sigma_so']   = r"$\sigma_{so}$",
+tex_names['f_so']       = r"$f_{so}$", 
+tex_names['b1sigma8']   = r"$b_1 \sigma_8$"

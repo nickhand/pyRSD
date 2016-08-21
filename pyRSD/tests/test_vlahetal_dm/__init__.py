0b8048c145d1204c56e75a570e0f4e7415267275
Nick Hand <nhand@berkeley.edu>
2016-08-20 22:36:03 -0700

    parametrize tests over redshift

--- pyRSD/tests/test_vlahetal_dm/__init__.py
+++ pyRSD/tests/test_vlahetal_dm/__init__.py
@@ -1,48 +1,53 @@
-from .. import unittest, pyplot as plt, cache_manager
+from .. import unittest, pytest
+from .. import pyplot as plt, cache_manager
 import os
 
-class TestVlahetalDM(unittest.TestCase):
+redshifts = [0., 0.509, 0.989, 2.070]
+
+@pytest.fixture(scope='module', params=redshifts)
+def redshift(request):
+    return request.param
+
+@pytest.fixture(scope='module')
+def model(redshift):
+        
+    from pyRSD.rsd import DarkMatterSpectrum
+    
+    # inititalize the model
+    config                   = {}
+    config['include_2loop']  = True
+    config['transfer_fit']   = 'CLASS'
+    config['cosmo_filename'] = 'teppei_sims.ini'
+    config['max_mu']         = 6
+    config['use_P00_model']  = False
+    config['use_P01_model']  = False
+    config['use_P11_model']  = False
+    config['use_Pdv_model']  = False
+    config['interpolate']    = False
+    config['z']              = redshift
+    m = DarkMatterSpectrum(**config)
+    
+    # load the model
+    with cache_manager(m, "vlahetal_dm_%.3f.npy" %redshift) as model:
+        
+        # set klims
+        model.kmin = 0.01
+        model.kmax = 0.5
+    
+    return model
+
+class TestVlahetalDM(object):
     """
     Test `DarkMatterSpectrum` by reproducing the figures in 
     Vlah et al. 2012
     """
-    def setUp(self):
-        
-        from pyRSD.rsd import DarkMatterSpectrum
-        
-        # inititalize the model
-        config                   = {}
-        config['include_2loop']  = True
-        config['transfer_fit']   = 'CLASS'
-        config['cosmo_filename'] = 'teppei_sims.ini'
-        config['max_mu']         = 6
-        config['use_P00_model']  = False
-        config['use_P01_model']  = False
-        config['use_P11_model']  = False
-        config['use_Pdv_model']  = False
-        config['interpolate']    = False
-        model = DarkMatterSpectrum(**config)
-        
-        # load the model
-        with cache_manager(model, "vlahetal_dm.npy") as m:
-            self.model = m
-            
-        # set kmax
-        self.model.kmax = 0.4
-        
-        # hi-res interpolation
-        self.model.Nk = 500
-            
-        # redshift info
-        self.redshifts = [0., 0.509, 0.989, 2.070]
-        
-    def update_redshift(self, z):
+    def update_redshift(self, thisz, model):
         """
         Update the redshift-dependent quantities
         """
-        self.model.z        = z
-        self.model.f        = self.model.cosmo.f_z(z)
-        self.model.sigma8_z = self.model.cosmo.Sigma8_z(z)
+        model.z        = thisz
+        model.f        = model.cosmo.f_z(thisz)
+        model.sigma8_z = model.cosmo.Sigma8_z(thisz)
     
     def new_axes(self, ylabel, xlims, ylims):
         """
@@ -66,7 +71,7 @@ class TestVlahetalDM(unittest.TestCase):
         
         return fig, ax
     
-    def savefig(self, fig, testname, z):
+    def savefig(self, fig, testname, z, dpi=1000):
         """
         Save the input figure
         """
@@ -77,9 +82,9 @@ class TestVlahetalDM(unittest.TestCase):
             
         # save
         filename = os.path.join(d, 'z_%.3f.jpg' %z)
-        fig.savefig(filename, dpi=1000)
+        fig.savefig(filename, dpi=dpi)
     
-    def test_P00(self):
+    def test_P00(self, redshift, model):
         """
         Reproduce Figure 1.
         """
@@ -87,27 +92,164 @@ class TestVlahetalDM(unittest.TestCase):
         xlims = (0., 0.3)
         ylims = (0.8, 1.5)
         ylabel = r"$P_{00}^\mathrm{DM} \ / P_\mathrm{NW}$"
-                
-        # make a figure for each redshift
-        for z in self.redshifts:
-        
-            fig, ax = self.new_axes(ylabel, xlims, ylims)
-            self.update_redshift(z)
-            
-            # normalization
-            norm = self.model.normed_power_lin_nw(self.model.k)
 
-            # plot the model (1-loop SPT)
-            kws = {'c':'b', 'ls':'-', 'label':"1-loop SPT"}
-            plt.plot(self.model.k, self.model.P00.total.mu0 / norm, **kws)
+        # new axes
+        fig, ax = self.new_axes(ylabel, xlims, ylims)
+        self.update_redshift(redshift, model)
+        
+        # normalization
+        norm = model.normed_power_lin_nw(model.k)
+
+        # plot the model (1-loop SPT)
+        kws = {'c':'b', 'ls':'-', 'label':"1-loop SPT"}
+        plt.plot(model.k, model.P00.total.mu0 / norm, **kws)
+        
+        # linear
+        kws = {'c':'k', 'ls':':', 'label':"linear"}
+        plt.plot(model.k, model.normed_power_lin(model.k) / norm, **kws)
+        
+        # zeldovich
+        kws = {'c':'Magenta', 'ls':'dashdot', 'label':"Zel'dovich"}
+        plt.plot(model.k, model.P00_hzpt_model.__zeldovich__(model.k) / norm, **kws)
+        
+        ax.legend(loc=0, fontsize=16)
+        self.savefig(fig, 'test_P00', redshift)
+            
+    def test_P01(self, redshift, model):
+        """
+        Reproduce Figure 2.
+        """
+        # axis limits
+        xlims = (0., 0.3)
+        ylims = (0.8, 1.5)
+        ylabel = r"$P_{01}^\mathrm{DM} \ / P_\mathrm{NW}$"
+
+        # new axes
+        fig, ax = self.new_axes(ylabel, xlims, ylims)
+        self.update_redshift(redshift, model)
+
+        # normalization
+        A = 2*model.f
+        norm = A*model.normed_power_lin_nw(model.k)
+
+        # plot the model (1-loop SPT)
+        kws = {'c':'b', 'ls':'-', 'label':"1-loop SPT"}
+        plt.plot(model.k, model.P01.total.mu2 / norm, **kws)
+
+        # linear
+        kws = {'c':'k', 'ls':':', 'label':"linear"}
+        plt.plot(model.k, A*model.normed_power_lin(model.k) / norm, **kws)
+
+        # zeldovich
+        kws = {'c':'Magenta', 'ls':'dashdot', 'label':"Zel'dovich"}
+        plt.plot(model.k, A*model.P01_hzpt_model.__zeldovich__(model.k) / norm, **kws)
+
+        ax.legend(loc=0, fontsize=16)
+        self.savefig(fig, 'test_P01', redshift)
+
+    def test_scalar_P11(self, redshift, model):
+        """
+        Reproduce Figure 3.
+        """
+        # axis limits
+        xlims = (0., 0.2)
+        ylims = (0.8, 2.0)
+        ylabel = r"$P_{11}^\mathrm{DM}[\mu^4] \ / P_\mathrm{NW}$"
+
+        # new axes
+        fig, ax = self.new_axes(ylabel, xlims, ylims)
+        
+        # 1-loop results
+        with model.preserve():
+            
+            model.include_2loop = False
+            self.update_redshift(redshift, model)
+
+            # normalization
+            A = model.f**2
+            norm = A * model.normed_power_lin_nw(model.k)
+
+            # plot the model
+            kws = {'c':'b', 'ls':'-', 'label':"model"}
+            plt.plot(model.k, model.P11.scalar.mu4 / norm, **kws)
+
+            # linear
+            kws = {'c':'k', 'ls':':', 'label':"linear"}
+            plt.plot(model.k, A*model.normed_power_lin(model.k) / norm, **kws)
+
+            ax.legend(loc=0, fontsize=16)
+            self.savefig(fig, 'test_scalar_P11', redshift)
+            
+    def test_P11(self, redshift, model):
+        """
+        Reproduce Figure 4.
+        """
+        # axis limits
+        xlims = (0.01, 0.5)
+        ylims = (1.0, 1e4)
+        ylabel = r"$P_{11}^{ss} \ \mathrm{[Mpc/h]^3}$"
+
+        # new axes
+        fig, ax = self.new_axes(ylabel, xlims, ylims)
+
+        # 1-loop results
+        with model.preserve():
+  
+            # update z
+            self.update_redshift(redshift, model)
+
+            # 1-loop scalar
+            model.include_2loop = False
+            plt.loglog(model.k, model.P11.scalar.mu4, label='1-loop, scalar', c='b') 
+            
+            # 1-loop and 2-loop vector
+            model.include_2loop = True
+            plt.loglog(model.k, model.P11.vector.mu2, label='2-loop, vector', color='r') 
+            model.include_2loop = False
+            plt.loglog(model.k, model.P11.vector.mu2, label='1-loop, vector', color='r', ls='--')
+
+            # 1-loop and 2-loop C11
+            plt.loglog(model.k, model.f**2 * model.I13(model.k), label=r'1-loop $C_{11}[\mu^4]$', c='g')
+            I1 = model.Ivvdd_h02(model.k)
+            I2 = model.Idvdv_h04(model.k)
+            plt.loglog(model.k, model.f**2 * (I1+I2), label=r'2-loop $C_{11}[\mu^4]$', c='g', ls='--')
             
             # linear
             kws = {'c':'k', 'ls':':', 'label':"linear"}
-            plt.plot(self.model.k, self.model.normed_power_lin(self.model.k) / norm, **kws)
+            plt.loglog(model.k, model.f**2*model.normed_power_lin(model.k), **kws)
+
+            ax.legend(loc=0, fontsize=16, ncol=2)
+            self.savefig(fig, 'test_P11', redshift)
             
-            # zeldovich
-            kws = {'c':'Magenta', 'ls':'dashdot', 'label':"Zel'dovich"}
-            plt.plot(self.model.k, self.model.P00_hzpt_model.__zeldovich__(self.model.k) / norm, **kws)
-            
-            ax.legend(loc=0, fontsize=16)
-            self.savefig(fig, 'test_P00', z)
+    # def test_P02(self, redshift, model):
+    #     """
+    #     Reproduce Figure 5.
+    #     """
+    #     # axis limits
+    #     xlims = (0.1, 0.5)
+    #     ylims = (-8, 2)
+    #     ylabel = r"$P_{11}^\mathrm{DM}[\mu^4] \ / P_\mathrm{NW}$"
+    #
+    #     # new axes
+    #     fig, ax = self.new_axes(ylabel, xlims, ylims)
+    #
+    #     # 1-loop results
+    #     with model.preserve():
+    #
+    #         model.include_2loop = False
+    #         self.update_redshift(redshift, model)
+    #
+    #         # normalization
+    #         A = model.f**2
+    #         norm = A * model.normed_power_lin_nw(model.k)
+    #
+    #         # plot the model
+    #         kws = {'c':'b', 'ls':'-', 'label':"model"}
+    #         plt.plot(model.k, model.P11.scalar.mu4 / norm, **kws)
+    #
+    #         # linear
+    #         kws = {'c':'k', 'ls':':', 'label':"linear"}
+    #         plt.plot(model.k, A*model.normed_power_lin(model.k) / norm, **kws)
+    #
+    #         ax.legend(loc=0, fontsize=16)
+    #         self.savefig(fig, 'test_P02', redshift)

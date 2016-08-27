68fc490b261e19ddfd6de906faccac498d36755c
Nick Hand <nhand@berkeley.edu>
2016-08-27 16:50:23 -0700

    set the global driver instance with a singleton pattern

--- pyRSD/rsdfit/__init__.py
+++ pyRSD/rsdfit/__init__.py
@@ -1,6 +1,28 @@
 params_filename = 'params.dat'
 model_filename = 'model.pickle'
 
+class GlobalFittingDriver(object):
+    """
+    The global :class:`~pyRSD.rsdfit.driver.FittingDriver` instance
+    """
+    _instance = None 
+    
+    @classmethod
+    def get(cls):
+        """
+        Get global driver, raising an exception if it is None
+        """
+        if cls._instance is None:
+            raise ValueError("global driver has not been set yet")
+        return cls._instance
+        
+    @classmethod
+    def set(cls, driver):
+        """
+        Set the global driver to the input value
+        """
+        cls._instance = driver
+
 import logging
 from .util.rsd_logging import MPILoggerAdapter
 
@@ -14,6 +36,3 @@ from . import parameters
 from . import results
 from . import theory
 from . import util
-
-
-__all__ = []
--- pyRSD/rsdfit/solvers/objectives.py
+++ pyRSD/rsdfit/solvers/objectives.py
@@ -1,32 +1,30 @@
-_driver = None
-def get_rsdfit_driver():
-    return _driver
-    
-def set_rsdfit_driver(d):
-    global _driver
-    _driver = d
-    
+from pyRSD.rsdfit import GlobalFittingDriver
+
 def neg_lnlike(x=None):
     """
     Wrapper for the negative log-likelihood
     """
-    return _driver.neg_lnlike(x, use_priors=False)
+    driver = GlobalFittingDriver.get()
+    return driver.neg_lnlike(x, use_priors=False)
     
 def neg_lnprob(x=None):
     """
     Wrapper for the negative log-probability (including priors)
     """
-    return _driver.neg_lnlike(x, use_priors=True)
+    driver = GlobalFittingDriver.get()
+    return driver.neg_lnlike(x, use_priors=True)
 
 def lnprob(x=None):
     """
     Wrapper for the log-probability (including priors)
     """
-    return _driver.lnprob(x)
+    driver = GlobalFittingDriver.get()
+    return driver.lnprob(x)
             
 def gradient(x, **kwargs):
     """
     Wrapper for ``FittingDriver.gradient`` which explictly
     grabs the pickeable ``nlopt_lnlike``
     """
-    return _driver.gradient(neg_lnlike, x, **kwargs)
+    driver = GlobalFittingDriver.get()
+    return driver.gradient(x, **kwargs)

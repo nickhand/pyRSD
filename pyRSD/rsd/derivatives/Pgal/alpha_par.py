b659f7f26d7d5d9878584ee964359e4e1067f25b
Nick Hand <nhand@berkeley.edu>
2016-08-28 23:09:35 -0700

    fixes the derivative expressions for alpha_par and alpha_perp

--- pyRSD/rsd/derivatives/Pgal/alpha_par.py
+++ pyRSD/rsd/derivatives/Pgal/alpha_par.py
@@ -7,24 +7,32 @@ class dPgal_dalpha_par(PgalDerivative):
     param = 'alpha_par'
     
     @staticmethod
-    def eval(m, pars, k, mu, epsilon=1e-3):
+    def eval(m, pars, k, mu, epsilon=1e-4):
         
+        # some setup
         aperp = m.alpha_perp
         apar  = m.alpha_par
+        F     = apar/aperp
+        N     = (1. + mu**2 * (1/F**2 - 1.))
         
         # derivative of the volume factor
         term1 = - m.Pgal(k, mu) / apar
         
-        # central finite difference derivative wrt to k,mu
-        gradk  = (m.Pgal(k+epsilon, mu) - m.Pgal(k-epsilon, mu)) / (2.*epsilon)
-        gradmu = (m.Pgal(k, mu+epsilon) - m.Pgal(k, mu-epsilon)) / (2.*epsilon)
-         
+        # first compute derivatives wrt to k, mu
+        dPgal_dk = (m.Pgal(k+epsilon, mu) - m.Pgal(k-epsilon, mu)) / (2.*epsilon)
+        dPgal_dmu = (m.Pgal(k, mu+epsilon) - m.Pgal(k, mu-epsilon)) / (2.*epsilon)
+        
+        # dPgal / dkprime
+        dkprime_dk  = N**0.5 / aperp
+        dPgal_dkprime  = dPgal_dk / dkprime_dk
+    
+        # compute dPgal / dmuprime
+        dkprime_dmu    = (k*mu/aperp) * (1/F**2 -1.) / N**0.5
+        dmuprime_dmu   = 1/F/N**1.5
+        dPgal_dmuprime = (dPgal_dmu - dPgal_dkprime*dkprime_dmu) / dmuprime_dmu
+        
         # derivative of AP remapping   
-        apar2 = apar**2; aperp2 = aperp**2
-        dk_da  = -aperp*k*mu**2 / apar**3 / (1. + mu**2 * (aperp2/apar2 - 1.))**0.5
-        dmu_da = aperp*mu*(1. - aperp*mu**2) / (-apar2 + (apar2-aperp2)*mu**2) /(1. + mu**2 * (aperp2/apar2 - 1.))**0.5
+        dkprime_da  = -k*mu**2 / (F*apar**2) / N**0.5
+        dmuprime_da = -mu*(1. - mu**2) / (F * apar * N**1.5)
         
-        # combine the 2nd term with chain rule
-        term2 = 1./aperp2/apar *  (gradk * dk_da + gradmu*dmu_da)
-        
-        return term1 + term2
+        return term1 + dPgal_dkprime * dkprime_da + dPgal_dmuprime * dmuprime_da
--- pyRSD/rsd/derivatives/Pgal/alpha_perp.py
+++ pyRSD/rsd/derivatives/Pgal/alpha_perp.py
@@ -1,4 +1,5 @@
 from . import PgalDerivative
+from pyRSD.rsd import APLock
 
 class dPgal_dalpha_perp(PgalDerivative):
     """
@@ -7,24 +8,32 @@ class dPgal_dalpha_perp(PgalDerivative):
     param = 'alpha_perp'
     
     @staticmethod
-    def eval(m, pars, k, mu, epsilon=1e-3):
+    def eval(m, pars, k, mu, epsilon=1e-4):
         
+        # some setup
         aperp = m.alpha_perp
         apar  = m.alpha_par
-        
+        F     = apar/aperp
+        N     = (1. + mu**2 * (1/F**2 - 1.))  
+                
         # derivative of the volume factor
         term1 = -2. * m.Pgal(k, mu) / aperp
         
-        # central finite difference derivative wrt to k,mu
-        gradk = (m.Pgal(k+epsilon, mu) - m.Pgal(k-epsilon, mu)) / (2.*epsilon)
-        gradmu = (m.Pgal(k, mu+epsilon) - m.Pgal(k, mu-epsilon)) / (2.*epsilon)
-         
-        # derivative of AP remapping   
-        apar2 = apar**2; aperp2 = aperp**2
-        dk_da  = k * (mu**2 - 1) / aperp2 / (1. + mu**2 * (aperp2/apar2 - 1.))**0.5
-        dmu_da = apar * mu * (mu**2 - 1.) / (-apar2 + (apar2-aperp2)*mu**2) /(1. + mu**2 * (aperp2/apar2 - 1.))**0.5
+        # first compute derivatives wrt to k, mu
+        dPgal_dk = (m.Pgal(k+epsilon, mu) - m.Pgal(k-epsilon, mu)) / (2.*epsilon)
+        dPgal_dmu = (m.Pgal(k, mu+epsilon) - m.Pgal(k, mu-epsilon)) / (2.*epsilon)
+            
+        # dPgal / dkprime
+        dkprime_dk  = N**0.5 / aperp
+        dPgal_dkprime  = dPgal_dk / dkprime_dk
+    
+        # compute dPgal / dmuprime
+        dkprime_dmu    = (k*mu/aperp) * (1/F**2 -1.) / N**0.5
+        dmuprime_dmu   = 1/F/N**1.5
+        dPgal_dmuprime = (dPgal_dmu - dPgal_dkprime*dkprime_dmu) / dmuprime_dmu
         
-        # combine the 2nd term with chain rule
-        term2 = (gradk*dk_da + gradmu*dmu_da)
-        
-        return term1 + term2
+        # derivative of remappings wrt alpha_perp  
+        dkprime_da = -k*(1. - mu**2) / aperp**2 / N**0.5
+        dmuprime_da = mu * (1. - mu**2) / (apar * N**1.5)
+
+        return term1 + dPgal_dkprime * dkprime_da + dPgal_dmuprime * dmuprime_da

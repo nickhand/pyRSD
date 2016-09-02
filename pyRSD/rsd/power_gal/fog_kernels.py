6b86c26caa638efaf33ceca477dc93aa7f0eb221
Nick Hand <nhand@berkeley.edu>
2016-09-02 00:19:27 -0700

    don't do AP shift explicitly in FOG kernel

--- pyRSD/rsd/power_gal/fog_kernels.py
+++ pyRSD/rsd/power_gal/fog_kernels.py
@@ -6,43 +6,32 @@ class FOGKernel(object):
     Factor class for returning a specific `FOGKernel`
     """
     @staticmethod
-    def factory(model, name):
+    def factory(name):
         if name == "modified_lorentzian": 
-            return ModifiedLorentizanKernel(model)
+            return ModifiedLorentizanKernel()
         elif name == "lorentzian": 
-            return LorentzianKernel(model)
+            return LorentzianKernel()
         elif name == 'gaussian':
-            return GaussianKernel(model)
+            return GaussianKernel()
         else:
             raise TypeError("no FOG kernel with name '%s'" %name) 
-
-    def __init__(self, model):
-        self.model = model
         
-    def __getattr__(self, name):
-        if hasattr(self.model, name):
-            return getattr(self.model, name)
-    
     @tools.broadcast_kmu
-    @tools.alcock_paczynski
     def __call__(self, k, mu, sigma):
         x = k*mu*sigma
         return self.__kernel__(x)
     
     @tools.broadcast_kmu
-    @tools.alcock_paczynski
     def derivative_k(self, k, mu, sigma):
         x = k*mu*sigma
         return self.__derivative__(x) * mu*sigma
         
     @tools.broadcast_kmu
-    @tools.alcock_paczynski
     def derivative_mu(self, k, mu, sigma):
         x = k*mu*sigma
         return self.__derivative__(x) * k*sigma
         
     @tools.broadcast_kmu
-    @tools.alcock_paczynski
     def derivative_sigma(self, k, mu, sigma):
         x = k*mu*sigma
         return self.__derivative__(x) * k*mu
@@ -55,7 +44,7 @@ class ModifiedLorentizanKernel(FOGKernel):
     .. math::
     
         G(x) = 1 / (1 + 0.5 x^2)^2
-    """
+    """        
     def __kernel__(self, x):
         return 1./(1 + 0.5*x**2)**2
         

91cd82f614d851b25bdd5bdca00514a488d2e6a1
Nick Hand <nhand@berkeley.edu>
2016-09-03 13:49:42 -0700

    add tests of the alphas that pass

--- pyRSD/tests/test_derivatives/test_alpha_perp.py
+++ pyRSD/tests/test_derivatives/test_alpha_perp.py
@@ -1,38 +1,11 @@
 from . import numdifftools, numpy
 import pytest
-from pyRSD.rsd.derivatives.Pgal import dPgal_dfs
 from pyRSD.rsd.derivatives.gradient import compute
 
 @pytest.fixture(scope='module', params=[True, False])
 def socorr(request):
     return request.param
-    
-def test_partial(driver, socorr):
-    
-    # set the socorr
-    driver.theory.model.use_so_correction = socorr
-    model = driver.theory.model
-    driver.set_fiducial()
-    
-    # get the deriv arguments
-    k    = driver.data.combined_k
-    mu   = driver.data.combined_mu
-    pars = driver.theory.fit_params
-    args = (model, pars, k, mu)
-    
-    # our derivative
-    x = dPgal_dfs.eval(*args)
-    
-    # numerical derivative
-    def f(x):
-        model.fs = x
-        return driver.theory.model.Pgal(k, mu)
-    g = numdifftools.Derivative(f, step=1e-3)
-    y = g(model.fs)
-    
-    # compare
-    numpy.testing.assert_allclose(x, y, rtol=1e-2)
-    
+        
 def test_total(driver, socorr):
     
     # set the socorr
@@ -47,10 +20,10 @@ def test_total(driver, socorr):
     args = (model, pars, k, mu)
     
     # our derivative
-    x = compute('fs', *args)
+    x = compute('alpha_perp', *args)
     
     # setup the numerical derivative
-    index = driver.theory.free_names.index('fs')
+    index = driver.theory.free_names.index('alpha_perp')
     theta = [driver.theory.fit_params[k].value for k in driver.theory.free_names]
     
     def f(x):

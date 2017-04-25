385f540caad41aca985466a80de7c3507256e856
Nick Hand <nhand@berkeley.edu>
2017-04-25 00:44:42 -0700

    up the tolerance a bit

--- pyRSD/tests/test_nonlinear_biasing.py
+++ pyRSD/tests/test_nonlinear_biasing.py
@@ -39,7 +39,7 @@ def get_nonlinear_biases(z_str, mass):
 #-------------------------------------------------------------------------------
 # TESTS
 #-------------------------------------------------------------------------------
-@pytest.mark.mpl_image_compare(style='seaborn-ticks', tolerance=20)
+@pytest.mark.mpl_image_compare(style='seaborn-ticks', tolerance=25)
 def test_nonlinear_biasing():
     """
     Compare the nonlinear biasing interpolation to that of Vlah et al. 2013

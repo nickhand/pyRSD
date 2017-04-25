569936f6e7b21ed6d2293d2613c6ba93125a2729
Nick Hand <nhand@berkeley.edu>
2017-04-24 21:16:04 -0700

    make instance methods in cache pickeable

--- pyRSD/rsd/_cache.py
+++ pyRSD/rsd/_cache.py
@@ -7,6 +7,23 @@ import inspect
 import fnmatch
 from six import add_metaclass, PY3, string_types
 
+try:
+    from functools import lru_cache
+except ImportError:
+    from pyRSD.extern.backports.lru_cache import lru_cache
+
+import types
+
+if not PY3:
+    def _pickle_method(m):
+        if m.im_self is None:
+            return getattr, (m.im_class, m.im_func.func_name)
+        else:
+            return getattr, (m.im_self, m.im_func.func_name)
+
+    import copyreg
+    copyreg.pickle(types.MethodType, _pickle_method)
+
 def doublewrap(f):
     """
     A decorator decorator, allowing the decorator to be used as:
@@ -59,12 +76,12 @@ class ParameterProperty(Property):
     """
     def __set__(self, obj, value):
         """
-        Explicitly pass the list of attributes on this 
+        Explicitly pass the list of attributes on this
         """
         if self.fset is None:
             raise AttributeError("can't set attribute")
         self.fset(obj, value, deps=self._deps)
-        
+
 class CachedProperty(Property):
     """
     A subclass of the `property` descriptor to represent an
@@ -72,17 +89,17 @@ class CachedProperty(Property):
     `CachedProperty` values, and can be cached appropriately
     """
     __cache__ = True
-    
+
     def __delete__(self, obj):
         if self.fdel is None:
             raise AttributeError("can't delete attribute")
         self.fdel(obj)
-        
+
         # clear the cache of any parameters that depend
         # on this cached property attribute
         for dep in self._deps:
             obj._cache.pop(dep, None)
-            
+
 class CacheSchema(type):
     """
     Metaclass to gather all `parameter` and `cached_property`
@@ -90,10 +107,10 @@ class CacheSchema(type):
     for caching purposes
     """
     def __init__(cls, clsname, bases, attrs):
-                                    
+
         # keep track of allowable kwargs of main class
         cls.allowable_kwargs = set()
-        if PY3:            
+        if PY3:
             sig = inspect.signature(cls.__init__)
             for name, p in sig.parameters.items():
                 if p.default != inspect.Parameter.empty:
@@ -101,23 +118,23 @@ class CacheSchema(type):
         else:
             attrs, varargs, varkw, defaults = inspect.getargspec(cls.__init__)
             cls.allowable_kwargs = set(attrs[1:])
-        
+
         # attach the registry attributes
         cls._cachemap = OrderedDict()
         cls._cached_names = set()
         cls._param_names = set()
-        
-        # for each class and base classes, track ParameterProperty 
+
+        # for each class and base classes, track ParameterProperty
         # and CachedProperty attributes
         classes = inspect.getmro(cls)
         for c in reversed(classes):
-            
+
             # add in allowable keywords from base classes
             if c != cls and hasattr(c, 'allowable_kwargs'):
-                cls.allowable_kwargs |= c.allowable_kwargs  
-            
+                cls.allowable_kwargs |= c.allowable_kwargs
+
             # loop over each attribute
-            for name in c.__dict__:                
+            for name in c.__dict__:
                 value = c.__dict__[name]
                 # track cached property
                 if getattr(value, '__cache__', False):
@@ -128,20 +145,20 @@ class CacheSchema(type):
                     if c == cls and fnmatch.fnmatch(name, 'use_*_model'):
                         cls.allowable_kwargs.add(name)
                     cls._param_names.add(name)
-            
+
         # invert the cache map
         def invert_cachemap(name, deps):
             """
             Recursively find all cached properties
             that depend on a given parameter
             """
-            for param in deps:                
-                
+            for param in deps:
+
                 # search classes in order for the attribute
                 for cls in classes:
                     f = getattr(cls, param, None)
                     if f is not None: break
-                    
+
                 # if a parameter, add to the deps
                 if isinstance(f, ParameterProperty):
                     f._deps.add(name)
@@ -155,22 +172,22 @@ class CacheSchema(type):
                         f._deps.add(name)
                     else:
                         raise ValueError("invalid parent property '%s' for cached property '%s'" %(param, name))
-                    
+
         # compute the inverse cache
         for name in cls._cachemap:
             invert_cachemap(name, cls._cachemap[name])
-    
+
 @add_metaclass(CacheSchema)
 class Cache(object):
     """
     The main class to do handle caching of parameters; this is the
     class that should serve as the base class
-    """    
+    """
     def __new__(cls, *args, **kwargs):
         obj = object.__new__(cls)
         obj._cache = {}
         return obj
-    
+
     def __init__(self, *args, **kwargs):
         super(Cache, self).__init__(*args, **kwargs)
 
@@ -191,7 +208,7 @@ def obj_eq(new_val, old_val):
     except:
         pass
     return equal
-    
+
 @doublewrap
 def parameter(f, default=None):
     """
@@ -208,20 +225,20 @@ def parameter(f, default=None):
         except AttributeError:
             old_val = None
             doset = True
-        
+
         if doset or not obj_eq(val, old_val):
             setattr(self, _name, val)
-        
+
             # clear the cache of any parameters that depend
             # on this attribute
             for dep in deps:
                 self._cache.pop(dep, None)
         return val
-        
+
     @functools.wraps(f)
     def _get_property(self):
         if _name not in self.__dict__:
-            
+
             if default is not None:
                 if isinstance(default, string_types):
                     if hasattr(self, default):
@@ -234,15 +251,15 @@ def parameter(f, default=None):
                     return f(self, default)
             else:
                 raise ValueError("required parameter '%s' has not yet been set" %name)
-        else:    
+        else:
             return self.__dict__[_name]
-            
+
     def _del_property(self):
         if _name in self.__dict__:
             self.__dict__.pop(_name)
         else:
             raise ValueError("cannot delete attribue '%s'" %name)
-       
+
     prop = ParameterProperty(_get_property, _set_property, _del_property)
     prop._deps = set() # track things that depend on this parameter
     return prop
@@ -252,53 +269,53 @@ def cached_property(*parents, **kws):
     Decorator to represent a model parameter will be cached
     and automatically updated if any of its dependencies change
     """
-    lru_cache = kws.pop('lru_cache', False)
+    _lru_cache = kws.pop('lru_cache', False)
     maxsize = kws.pop('maxsize', 128)
-    
+
     def cache(f):
         name = f.__name__
-        
+
         @functools.wraps(f)
         def _get_property(self):
-            
+
             # check for user overrides
             if name in getattr(self, "_cache_overrides", {}):
                 return self._cache_overrides[name]
-            
-            # add to cache 
+
+            # add to cache
             if name not in self._cache:
                 val = f(self)
-                if lru_cache and callable(val):
-                    val = functools.lru_cache(maxsize=maxsize)(val)
+                if _lru_cache and callable(val):
+                    val = lru_cache(maxsize=maxsize)(val)
                 self._cache[name] = val
-            
+
             # return the cached value
             return self._cache[name]
-        
+
         def _del_property(self):
             self._cache.pop(name, None)
-                                    
+
         prop = CachedProperty(_get_property, None, _del_property)
         prop._parents = list(parents) # the dependencies of this property
         prop._deps = set()
-        
-        prop._lru_cache = lru_cache
+
+        prop._lru_cache = _lru_cache
         prop._maxsize   = maxsize
-        
+
         return prop
     return cache
 
 class InterpolatedFunction(object):
     """
-    A callable class (as a function of wavenumber) that will 
-    either evaluate a spline or evaluate the underlying function, 
+    A callable class (as a function of wavenumber) that will
+    either evaluate a spline or evaluate the underlying function,
     if a domain error occurs
     """
     def __init__(self, spline, function, name):
         self.spline   = spline
         self.function = function
         self.name     = name
-    
+
     def __call__(self, k, derivative=False):
         try:
             if isinstance(self.spline, list):
@@ -312,7 +329,7 @@ class InterpolatedFunction(object):
                 else:
                     return self.spline.derivative()(k)
         except InterpolationDomainError:
-            
+
             # print a warning
             import warnings
             warnings.warn("evaluating interpolated function %s outside of domain...will be slow!!" %(self.name))
@@ -329,7 +346,7 @@ class InterpolatedFunction(object):
                 return toret
         except:
             raise
-        
+
 def interpolated_function(*parents, **kwargs):
     """
     A decorator that represents a cached property that
@@ -348,7 +365,7 @@ def interpolated_function(*parents, **kwargs):
             ignore_cache = kws.pop('ignore_cache', False)
             if ignore_cache:
                 return f(self, *args)
-                            
+
             # the spline isn't in the cache, make the spline
             if name not in self._cache:
 
@@ -356,10 +373,10 @@ def interpolated_function(*parents, **kwargs):
                 interp_domain = getattr(self, kwargs.get("interp", "k_interp"))
                 val = f(self, interp_domain)
                 spline_kwargs = getattr(self, 'spline_kwargs', {})
-                
+
                 # the function that will explicitly evaluate the original function
-                g = functools.partial(wrapped, self, ignore_cache=True)
-                
+                g = wrapped.__get__(self, self.__class__)
+
                 # tuple of splines
                 if isinstance(val, tuple):
                     splines = [self.spline(interp_domain, x, **spline_kwargs) for x in val]
@@ -368,14 +385,14 @@ def interpolated_function(*parents, **kwargs):
                 else:
                     spl = self.spline(interp_domain, val, **spline_kwargs)
                     self._cache[name] = InterpolatedFunction(spl, g, name)
-                    
+
             return self._cache[name](*args, **kws)
-            
+
         # store the meta information about this property
         wrapped._parents = list(parents) # the dependencies of this property
         wrapped._deps = set()
         wrapped.__cache__ = True
 
         return wrapped
-    
-    return wrapper        
+
+    return wrapper

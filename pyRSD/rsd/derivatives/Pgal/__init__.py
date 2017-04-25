eb3d352aae09fbfbb154607605aed7fbc83af42a
Nick Hand <nhand@berkeley.edu>
2017-04-24 21:16:28 -0700

    fix abc meta missing import in py 2

--- pyRSD/rsd/derivatives/Pgal/__init__.py
+++ pyRSD/rsd/derivatives/Pgal/__init__.py
@@ -1,6 +1,8 @@
 import abc
+from six import add_metaclass
 
-class PgalDerivative(abc.ABC):
+@add_metaclass(abc.ABCMeta)
+class PgalDerivative(object):
     """
     Abstract base class for derivatives of the `Pgal` model
     """
@@ -12,13 +14,14 @@ class PgalDerivative(abc.ABC):
         d = {}
         for subclass in cls.__subclasses__():
             d[subclass.param] = subclass
-            
+
         return d
-    
-    @abc.abstractstaticmethod
+
+    @staticmethod
+    @abc.abstractmethod
     def eval(model, pars, k, mu):
         pass
-        
+
 def get_Pgal_derivative(name):
     """
     Given an input parameter name, return the
@@ -28,8 +31,8 @@ def get_Pgal_derivative(name):
     if name not in registry:
         raise ValueError("no registered subclass for dPgal/d%s" %name)
     return registry[name]
-    
-    
+
+
 from .alpha_par  import dPgal_dalpha_par
 from .alpha_perp import dPgal_dalpha_perp
 from .f_so       import dPgal_df_so
@@ -40,7 +43,7 @@ from .NcBs       import dPgal_dNcBs
 from .NsBsB      import dPgal_dNsBsB
 from .sigma_c    import dPgal_dsigma_c
 from .sigma_so   import dPgal_dsigma_so
-from .nuisance   import (dPgal_dNsat_mult, dPgal_df1h_sBsB, 
+from .nuisance   import (dPgal_dNsat_mult, dPgal_df1h_sBsB,
                         dPgal_df1h_cBs, dPgal_dlog10_fso)
 
 

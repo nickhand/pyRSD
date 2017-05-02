fix the broadcast of QSO power

pyRSD/rsd/power/qso/power_qso.py
@@ -109,9 +109,8 @@ class QuasarSpectrum(HaloSpectrum):
         """
         return k*0.
 
-    @tools.broadcast_kmu
     @tools.alcock_paczynski
-    def power(self, k, mu, flatten=True):
+    def power(self, k, mu, flatten=False):
         """
         Return the redshift space power spectrum at the specified value of mu,
         including terms up to ``mu**self.max_mu``.
@@ -133,7 +132,7 @@ class QuasarSpectrum(HaloSpectrum):
             the returned array is raveled, with dimensions of `(N*len(self.k), )`
         """
         # the linear kaiser P(k,mu)
-        pkmu = self._power(k, mu)
+        pkmu = super(QuasarSpectrum, self).power(k, mu)
 
         # add FOG damping
         G = self.FOG(k, mu, self.sigma_fog)
@@ -145,7 +144,6 @@ class QuasarSpectrum(HaloSpectrum):
         if flatten: pkmu = np.ravel(pkmu, order='F')
         return pkmu
 
-    @tools.broadcast_kmu
     @tools.alcock_paczynski
     def derivative_k(self, k, mu):
         """
@@ -159,7 +157,6 @@ class QuasarSpectrum(HaloSpectrum):
 
         return G**2 * deriv + 2 * G*Gprime * power
 
-    @tools.broadcast_kmu
     @tools.alcock_paczynski
     def derivative_mu(self, k, mu):
         """

pyRSD/rsd/tools.py
@@ -248,8 +248,10 @@ def monopole(f):
     """
     @functools.wraps(f)
     def wrapper(self, *args, **kwargs):
-        k = args[0]
-        mus = np.linspace(0., 1., 101)
+        k = args[0]; Nmu = 41
+        if len(k) == Nmu: Nmu += 1
+        mus = np.linspace(0., 1., Nmu)
+        kwargs['flatten'] = False
         Pkmus = f(self, k, mus, **kwargs)
         return np.array([simps(pk, x=mus) for pk in Pkmus])
     return wrapper
@@ -260,8 +262,10 @@ def quadrupole(f):
     """
     @functools.wraps(f)
     def wrapper(self, *args, **kwargs):
-        k = args[0]
-        mus = np.linspace(0., 1., 101)
+        k = args[0]; Nmu = 41
+        if len(k) == Nmu: Nmu += 1
+        mus = np.linspace(0., 1., Nmu)
+        kwargs['flatten'] = False
         Pkmus = f(self, k, mus, **kwargs)
         kern = 2.5*(3*mus**2 - 1.)
         return np.array([simps(kern*pk, x=mus) for pk in Pkmus])
@@ -273,8 +277,10 @@ def hexadecapole(f):
     """
     @functools.wraps(f)
     def wrapper(self, *args, **kwargs):
-        k = args[0]
-        mus = np.linspace(0., 1., 1001)
+        k = args[0]; Nmu = 41
+        if len(k) == Nmu: Nmu += 1
+        mus = np.linspace(0., 1., Nmu)
+        kwargs['flatten'] = False
         Pkmus = f(self, k, mus, **kwargs)
         kern = 9./8.*(35*mus**4 - 30.*mus**2 + 3.)
         return np.array([simps(kern*pk, x=mus) for pk in Pkmus])
@@ -286,8 +292,10 @@ def tetrahexadecapole(f):
     """
     @functools.wraps(f)
     def wrapper(self, *args, **kwargs):
-        k = args[0]
-        mus = np.linspace(0., 1., 1001)
+        k = args[0]; Nmu = 41
+        if len(k) == Nmu: Nmu += 1
+        mus = np.linspace(0., 1., Nmu)
+        kwargs['flatten'] = False
         Pkmus = f(self, k, mus, **kwargs)
         kern = 13./16.*(231*mus**6 - 315*mus**4 + 105*mus**2 - 5)
         return np.array([simps(kern*pk, x=mus) for pk in Pkmus])